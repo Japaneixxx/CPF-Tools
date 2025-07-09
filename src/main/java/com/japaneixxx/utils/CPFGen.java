c831d306babde15c744defc5d13ea0a02baed423
Japaneixxx <japaneix@gmail.com>
2025-07-09 15:52:59 -0300

    Initial commit

--- src/main/java/com/japaneixxx/utils/CPFGen.java
+++ src/main/java/com/japaneixxx/utils/CPFGen.java
@@ -12,6 +12,16 @@ public class CPFGen {
         int n8 = (int) (Math.random() * 10);
 
         int n9 = Regiao;
+//          •1: Distrito Federal, Goiás, Mato Grosso, Mato Grosso do Sul e Tocantins
+//          •2: Amazonas, Pará, Roraima, Amapá, Acre e Rondônia
+//          •3: Ceará, Maranhão e Piauí
+//          •4: Paraíba, Pernambuco, Alagoas e Rio Grande do Norte
+//          •5: Bahia e Sergipe
+//          •6: Minas Gerais
+//          •7: Rio de Janeiro e Espírito Santo
+//          •8: São Paulo
+//          •9: Paraná e Santa Catarina
+//          •0: Rio Grande do Sul
 
 
         int d1 = n9 * 2 + n8 * 3 + n7 * 4 + n6 * 5 + n5 * 6 + n4 * 7 + n3 * 8 + n2 * 9 + n1 * 10;
